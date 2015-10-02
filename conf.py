0722b3ae15bdd9a74be0d770753b0fd0668f4ec6
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-10-02 10:57:30 +0200

    merged css theme overrides into theme css

--- conf.py
+++ conf.py
@@ -15,12 +15,6 @@ html_theme = 'connector_rtd_theme'
 html_theme_path = ["_themes"]
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-    ],
-}
-
 htmlhelp_basename = 'Connectordoc'
 man_pages = [
     ('index', 'connector', u'JTL-Connector Documentation',
